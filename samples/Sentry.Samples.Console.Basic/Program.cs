b029942d1f071ba9259982bd877e4bdad9d132be
Cengiz İçdem <cicdem@amvg.com>
2021-01-18 09:15:29 +0300

    Update Program.cs

--- samples/Sentry.Samples.Console.Basic/Program.cs
+++ samples/Sentry.Samples.Console.Basic/Program.cs
@@ -1,13 +1,16 @@
+using System;
 using Sentry;
 
 static class Program
 {
     static void Main()
     {
-        using (SentrySdk.Init("https://80aed643f81249d4bed3e30687b310ab@o447951.ingest.sentry.io/5428537"))
+        using (SentrySdk.Init("https://eb71c11712f7493ea868ea426744cf1b@o487614.ingest.sentry.io/5546564"))
         {
             // The following exception is captured and sent to Sentry
+
             throw null;
+
         }
     }
 }
